f7ed151dca57c2ca7816e0d8f6b0b402fa21fd84
keshucscs <keshucscs@gmail.com>
2020-11-26 19:55:49 +0530

    height and Size of BST

--- Tree/BinarySearchTree/binarySearchTree.go
+++ Tree/BinarySearchTree/binarySearchTree.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 // Node Structure
@@ -49,15 +50,37 @@ func (n *Node) Search(key int) bool {
 	return true
 }
 
+func GetSize(root *Node) int {
+	if root == nil {
+		return 0
+	} else {
+		return 1 + GetSize(root.left) + GetSize(root.right)
+	}
+}
+
+func GetHeight(root *Node) float64 {
+	if root == nil {
+		return 0
+	} else {
+		return math.Max(GetHeight(root.left), GetHeight(root.right)) + 1
+	}
+}
+
 func main() {
 	tree := &Node{data: 150}
 	fmt.Println(tree)
 	tree.Insert(100)
 	tree.Insert(300)
+	tree.Insert(500)
+	tree.Insert(600)
 	fmt.Println(tree.left)
 	fmt.Println(tree.right)
 	ok := tree.Search(300)
 	fmt.Println(ok)
 	ok = tree.Search(700)
 	fmt.Println(ok)
+
+	fmt.Println(GetSize(tree))
+	fmt.Println(GetHeight(tree))
+
 }
